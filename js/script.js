5e74ce73b7c027ee66b9abd2d730bf54165a7b35
oleoot <oleoot28@gmail.com>
2020-05-06 14:11:39 +0300

    Getting cart item prcie & quantity on remove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-let cartRow = document.querySelectorAll('.cart-row');
 let rmBtn = document.querySelectorAll('.btn-danger');
-console.log(rmBtn)
 
 
 
@@ -8,5 +6,18 @@ rmBtn.forEach((cartItem) => {
     cartItem.addEventListener('click', (event) => {
         let btnClicked = event.target;
         btnClicked.parentElement.parentElement.remove();
+        updateCartTotal()
     })
 })
+
+updateCartTotal = () => {
+    let cartItemContainer = document.querySelectorAll('.cart-items')[0];
+    let cartRows = cartItemContainer.querySelectorAll('.cart-row')
+    cartRows.forEach((cartRows) => {
+        let priceElement = cartRows.querySelectorAll('.cart-price')[0];
+        let quantityElement = cartRows.querySelectorAll('.cart-quantity-input')[0];
+        let price = +priceElement.innerText.replace('$', '')
+        let quantity = quantityElement.value
+        console.log(price, quantity)
+    })
+}
