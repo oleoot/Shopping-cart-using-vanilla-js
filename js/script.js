573c2a312efdb2c748c2de2708572d3599bd2288
oleoot <oleoot28@gmail.com>
2020-05-06 15:41:59 +0300

    changed js to plain functions, cart total updates with new items in cart

--- js/script.js
+++ js/script.js
@@ -1,43 +1,52 @@
 
 let rmBtn = document.querySelectorAll('.btn-danger');
-
 rmBtn.forEach((cartItem) => {
-    cartItem.addEventListener('click', (event) => {
-        let btnClicked = event.target;
-        btnClicked.parentElement.parentElement.remove();
-        updateCartTotal()
-    })
+    cartItem.addEventListener('click', removeCartItem)
 })
 
 let quantityInput = document.querySelectorAll('.cart-quantity-input')
 quantityInput.forEach((input) => {
-    input.addEventListener('change', (event) => {
-        let inputValue = event.target
-        if (isNaN(inputValue.value) || inputValue.value <= 0) {
-            inputValue.value = 1
-        }
-        updateCartTotal();
-    })
-
+    input.addEventListener('change', quantityChanged)
 })
 
 
 let addToCartBtn = document.querySelectorAll(".shop-item-button")
 addToCartBtn.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        let button = event.target
-        let shopItem = button.parentElement.parentElement
-        let title = shopItem.querySelectorAll('.shop-item-title')[0].innerText
-        let price = shopItem.querySelectorAll('.shop-item-price')[0].innerText
-        let imgSrc = shopItem.querySelectorAll('.shop-item-image')[0].src
-        addItemToCart(title, price, imgSrc)
-    })
+    button.addEventListener('click', addItemToCartClicked)
 })
 
-addItemToCart = (title, price, imgSrc) => {
+function removeCartItem(event) {
+    let btnClicked = event.target;
+    btnClicked.parentElement.parentElement.remove();
+    updateCartTotal()
+}
+function quantityChanged(event) {
+    let inputValue = event.target
+    if (isNaN(inputValue.value) || inputValue.value <= 0) {
+        inputValue.value = 1
+    }
+    updateCartTotal();
+}
+function addItemToCartClicked() {
+    let button = event.target
+    let shopItem = button.parentElement.parentElement
+    let title = shopItem.querySelectorAll('.shop-item-title')[0].innerText
+    let price = shopItem.querySelectorAll('.shop-item-price')[0].innerText
+    let imgSrc = shopItem.querySelectorAll('.shop-item-image')[0].src
+    addItemToCart(title, price, imgSrc)
+    updateCartTotal();
+}
+function addItemToCart(title, price, imgSrc) {
     let cartRow = document.createElement('div');
     cartRow.classList.add("cart-row")
     let cartItems = document.querySelectorAll('.cart-items')[0];
+    let cartItemNames = cartItems.querySelectorAll('.cart-item-title')
+    cartItemNames.forEach((item) => {
+        if (item.innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    })
     let cartRowContents = `
     <div class="cart-item cart-column">
     <img class="cart-item-image" src=${imgSrc} width="100" height="100">
@@ -50,9 +59,11 @@ addItemToCart = (title, price, imgSrc) => {
 </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
+    cartRow.querySelectorAll('.btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.querySelectorAll('.cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
-updateCartTotal = () => {
+function updateCartTotal() {
     let cartItemContainer = document.querySelectorAll('.cart-items')[0];
     let cartRows = cartItemContainer.querySelectorAll('.cart-row')
     let total = 0;
